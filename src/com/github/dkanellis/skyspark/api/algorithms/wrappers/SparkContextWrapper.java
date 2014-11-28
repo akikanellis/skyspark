dd6e4b6130ffa1d4e194333f1c5aea7a6b4826e8
dkanellis <gitkanellis@outlook.com>
2014-11-28 21:03:45 +0200

    added paralellize and stop

--- src/com/github/dkanellis/skyspark/api/algorithms/wrappers/SparkContextWrapper.java
+++ src/com/github/dkanellis/skyspark/api/algorithms/wrappers/SparkContextWrapper.java
@@ -1,7 +1,9 @@
 package com.github.dkanellis.skyspark.api.algorithms.wrappers;
 
+import java.util.List;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.*;
+import scala.Tuple2;
 
 /**
  *
@@ -9,19 +11,30 @@ import org.apache.spark.api.java.*;
  */
 public class SparkContextWrapper {
 
-    private final SparkConf sparConf;
+    private final SparkConf sparkConf;
     private final JavaSparkContext sparkContext;
 
     public SparkContextWrapper(String appName, String master) {
-        this.sparConf = new SparkConf();
-        this.sparConf.setAppName(appName);
-        this.sparConf.setMaster(master);
+        this.sparkConf = new SparkConf();
+        this.sparkConf.setAppName(appName);
+        this.sparkConf.setMaster(master);
 
-        this.sparkContext = new JavaSparkContext(sparConf);
+        this.sparkContext = new JavaSparkContext(sparkConf);
     }
 
     public JavaRDD<String> textFile(String path) {
         return sparkContext.textFile(path);
     }
-
+    
+    public <T> JavaRDD<T> parallelize(List<T> list) {
+        return sparkContext.parallelize(list);
+    }
+    
+    public <K,V> JavaPairRDD<K,V> parallelizePairs(List<Tuple2<K,V>> list){
+        return sparkContext.parallelizePairs(list);
+    }
+    
+    public void stop(){
+        sparkContext.stop();
+    }
 }
