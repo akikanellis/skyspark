df3822379c6a09bf1259aef679194ec07f3a4973
Aki Kanellis <gitkanellis@gmail.com>
2014-12-01 21:00:07 +0200

    organized imports

--- src/com/github/dkanellis/skyspark/main/Main.java
+++ src/com/github/dkanellis/skyspark/main/Main.java
@@ -1,18 +1,8 @@
 package com.github.dkanellis.skyspark.main;
 
-import com.github.dkanellis.skyspark.api.algorithms.factories.SkylineAlgorithmFactory;
-import com.github.dkanellis.skyspark.api.algorithms.sparkimplementations.SkylineAlgorithm;
-import com.github.dkanellis.skyspark.api.algorithms.wrappers.SparkContextWrapper;
-import com.github.dkanellis.skyspark.api.math.point.PointUtils;
-import com.github.dkanellis.skyspark.performance.PerformanceResult;
 import java.awt.geom.Point2D;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
-import static org.junit.Assert.assertTrue;
-import org.junit.runner.JUnitCore;
-import org.junit.runner.Result;
 
 /**
  *
