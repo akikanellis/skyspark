ed30070a57217dd3110eaa28282981030c0c87cc
Aki Kanellis <gitkanellis@gmail.com>
2014-12-01 20:58:33 +0200

    removed redundant main functions

--- src/com/github/dkanellis/skyspark/main/Main.java
+++ src/com/github/dkanellis/skyspark/main/Main.java
@@ -4,9 +4,15 @@ import com.github.dkanellis.skyspark.api.algorithms.factories.SkylineAlgorithmFa
 import com.github.dkanellis.skyspark.api.algorithms.sparkimplementations.SkylineAlgorithm;
 import com.github.dkanellis.skyspark.api.algorithms.wrappers.SparkContextWrapper;
 import com.github.dkanellis.skyspark.api.math.point.PointUtils;
+import com.github.dkanellis.skyspark.performance.PerformanceResult;
 import java.awt.geom.Point2D;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import static org.junit.Assert.assertTrue;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
 
 /**
  *
@@ -30,24 +36,7 @@ public class Main {
     private final static String UNIFORM_BIG = DATASETS_FOLDER + "UNIFORM_2_1000000.txt";
 
     public static void main(String[] args) {
-        SparkContextWrapper sparkContext = new SparkContextWrapper("Skyline solver", "local[4]");
-        SkylineAlgorithmFactory algorithmFactory = new SkylineAlgorithmFactory();
-        List<String> files = createFileList();
-        String file = ANTICORRELATED_BIG;
-
-        SkylineAlgorithm bnl = algorithmFactory.getBlockNestedLoop(sparkContext);
-        SkylineAlgorithm sfs = algorithmFactory.getSortFilterSkyline(sparkContext);
-        long startTime = System.nanoTime();
-        List<Point2D> skylinesBnl = bnl.getSkylinePoints(file);
-        long endTime = System.nanoTime();
-
-        long durationBnl = (endTime - startTime) / 1000000;
-        startTime = System.nanoTime();
-        List<Point2D> skylinesSfs = sfs.getSkylinePoints(file);
-        endTime = System.nanoTime();
-        long durationSfs = (endTime - startTime) / 1000000;
-        System.out.println("BNL: " + durationBnl + " ms - size: " + skylinesBnl.size());
-        System.out.println("SFS: " + durationSfs + " ms - size: " + skylinesSfs.size());
+        // Run one of the test suites
     }
 
     private static void print(List<Point2D> points) {
@@ -62,43 +51,6 @@ public class Main {
         for (int i = 0; i < first.size(); i++) {
             System.out.println(i + ": " + first.get(i) + " - " + second.get(i));
         }
-    }
-
-    public static boolean checkCorrectness(List<Point2D> points) {
-        for (int i = 0; i < points.size(); i++) {
-            Point2D p1 = points.get(i);
-            for (int j = 0; j < points.size(); j++) {
-                Point2D p2 = points.get(j);
-                if (PointUtils.dominates(p1, p2) || PointUtils.dominates(p2, p1)) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    public static boolean checkCorrectness(Iterable<Point2D> points) {
-        for (Point2D point : points) {
-            for (Point2D other : points) {
-                if (PointUtils.dominates(point, other) || PointUtils.dominates(other, point)) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    private static boolean areSame(List<Point2D> first, List<Point2D> second) {
-        if (first.size() != second.size()) {
-            return false;
-        }
-
-        for (Point2D point : first) {
-            if (!second.contains(point)) {
-                return false;
-            }
-        }
-        return true;
     }
 
     private static List<String> createFileList() {
