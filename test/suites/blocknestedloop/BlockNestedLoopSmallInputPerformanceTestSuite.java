7c44744e149b633c987734ef5e691145b033ffeb
Aki Kanellis <gitkanellis@gmail.com>
2014-12-02 12:46:39 +0200

    added full range of BNL test suites

---
+++ test/suites/blocknestedloop/BlockNestedLoopSmallInputPerformanceTestSuite.java
@@ -0,0 +1,31 @@
+package suites.blocknestedloop;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import performance.blocknestedloop.AbstractPerformanceTest;
+import performance.blocknestedloop.BlockNestedLoopPerformanceTest;
+
+/**
+ *
+ * @author Dimitris Kanellis
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({BlockNestedLoopPerformanceTest.class})
+public class BlockNestedLoopSmallInputPerformanceTestSuite {
+
+    private final static String DATASETS_FOLDER = "data/datasets/small";
+    private final static String EXPECTED_RESULTS_FOLDER = "data/expected results/small";
+
+    @BeforeClass
+    public static void setUpClass() {
+        AbstractPerformanceTest.init(DATASETS_FOLDER, EXPECTED_RESULTS_FOLDER, "BlockNestedLoopSmallInputPerformanceTestSuite");
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws InterruptedException {
+        AbstractPerformanceTest.stopSparkContext();
+        AbstractPerformanceTest.printResults();
+    }
+}
