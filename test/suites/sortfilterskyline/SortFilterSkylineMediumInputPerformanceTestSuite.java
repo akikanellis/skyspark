5767b4b95872d7af64df940977e783a8f45b2cf1
Aki Kanellis <gitkanellis@gmail.com>
2014-12-02 12:54:18 +0200

    added full range of SFS test suites

---
+++ test/suites/sortfilterskyline/SortFilterSkylineMediumInputPerformanceTestSuite.java
@@ -0,0 +1,34 @@
+package suites.sortfilterskyline;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import performance.blocknestedloop.AbstractPerformanceTest;
+import performance.blocknestedloop.SortFilterSkylinePerformanceTest;
+
+/**
+ *
+ * @author Dimitris Kanellis
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({SortFilterSkylinePerformanceTest.class})
+public class SortFilterSkylineMediumInputPerformanceTestSuite {
+
+    private final static String DATASETS_FOLDER = "data/datasets/medium";
+    private final static String EXPECTED_RESULTS_FOLDER = "data/expected results/medium";
+
+    @BeforeClass
+    public static void setUpClass() {
+        AbstractPerformanceTest.init(
+                DATASETS_FOLDER,
+                EXPECTED_RESULTS_FOLDER,
+                "SortFilterSkylineMediumInputPerformanceTestSuite");
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws InterruptedException {
+        AbstractPerformanceTest.stopSparkContext();
+        AbstractPerformanceTest.printResults();
+    }
+}
