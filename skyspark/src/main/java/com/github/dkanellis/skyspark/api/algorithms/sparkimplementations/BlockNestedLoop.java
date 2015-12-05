6aae4c92b4732c030dec19380f3a8dcf1697f9fd
Aki Kanellis <dkkanellis@gmail.com>
2015-12-05 20:59:41 +0000

    Cleanup local addDiscard

--- skyspark/src/main/java/com/github/dkanellis/skyspark/api/algorithms/sparkimplementations/BlockNestedLoop.java
+++ skyspark/src/main/java/com/github/dkanellis/skyspark/api/algorithms/sparkimplementations/BlockNestedLoop.java
@@ -3,11 +3,9 @@ package com.github.dkanellis.skyspark.api.algorithms.sparkimplementations;
 import com.github.dkanellis.skyspark.api.algorithms.templates.BlockNestedLoopTemplate;
 import com.github.dkanellis.skyspark.api.algorithms.wrappers.SparkContextWrapper;
 import com.github.dkanellis.skyspark.api.math.point.PointFlag;
-import com.github.dkanellis.skyspark.api.math.point.PointUtils;
 import org.apache.spark.api.java.JavaPairRDD;
 
 import java.awt.geom.Point2D;
-import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -26,15 +24,7 @@ public class BlockNestedLoop extends BlockNestedLoopTemplate {
 
     @Override
     protected void globalAddDiscardOrDominate(List<Point2D> globalSkylines, Point2D candidateGlobalSkylinePoint) {
-        for (Iterator it = globalSkylines.iterator(); it.hasNext(); ) {
-            Point2D skyline = (Point2D) it.next();
-            if (PointUtils.dominates(skyline, candidateGlobalSkylinePoint)) {
-                return;
-            } else if (PointUtils.dominates(candidateGlobalSkylinePoint, skyline)) {
-                it.remove();
-            }
-        }
-        globalSkylines.add(candidateGlobalSkylinePoint);
+        localAddDiscardOrDominate(globalSkylines, candidateGlobalSkylinePoint);
     }
 
 }
