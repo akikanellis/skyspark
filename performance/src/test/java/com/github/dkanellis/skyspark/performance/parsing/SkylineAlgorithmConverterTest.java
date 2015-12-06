387090eeca9a34f10a002ce195d6a9f52f5edf6f
Aki Kanellis <dkkanellis@gmail.com>
2015-12-06 16:14:09 +0000

    Add converter tests

--- performance/src/test/java/com/github/dkanellis/skyspark/performance/parsing/SkylineAlgorithmConverterTest.java
+++ performance/src/test/java/com/github/dkanellis/skyspark/performance/parsing/SkylineAlgorithmConverterTest.java
@@ -1,11 +1,48 @@
 package com.github.dkanellis.skyspark.performance.parsing;
 
+import com.beust.jcommander.ParameterException;
+import com.github.dkanellis.skyspark.api.algorithms.sparkimplementations.Bitmap;
+import com.github.dkanellis.skyspark.api.algorithms.sparkimplementations.BlockNestedLoop;
+import com.github.dkanellis.skyspark.api.algorithms.sparkimplementations.SkylineAlgorithm;
+import com.github.dkanellis.skyspark.api.algorithms.sparkimplementations.SortFilterSkyline;
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
 
 public class SkylineAlgorithmConverterTest {
 
-    @Test
-    public void testConvert() throws Exception {
+    SkylineAlgorithmConverter skylineAlgorithmConverter;
 
+    @Before
+    public void setUp() {
+        skylineAlgorithmConverter = new SkylineAlgorithmConverter();
+    }
+
+    @Test
+    public void bnl_returnBlockNestedLoop() {
+        SkylineAlgorithm skylineAlgorithm = skylineAlgorithmConverter.convert(SkylineAlgorithmConverter.ALIAS_BLOCK_NESTED_LOOP);
+
+        assertThat(skylineAlgorithm, instanceOf(BlockNestedLoop.class));
+    }
+
+    @Test
+    public void sfs_returnSortFilterSkyline() {
+        SkylineAlgorithm skylineAlgorithm = skylineAlgorithmConverter.convert(SkylineAlgorithmConverter.ALIAS_SORT_FILTER_SKYLINE);
+
+        assertThat(skylineAlgorithm, instanceOf(SortFilterSkyline.class));
+    }
+
+    @Test
+    public void bitmap_returnBitmap() {
+        SkylineAlgorithm skylineAlgorithm = skylineAlgorithmConverter.convert(SkylineAlgorithmConverter.ALIAS_BITMAP);
+
+        assertThat(skylineAlgorithm, instanceOf(Bitmap.class));
+    }
+
+    @Test(expected = ParameterException.class)
+    public void unknown_throwException() {
+        skylineAlgorithmConverter.convert("random string");
     }
 }
