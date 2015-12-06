b6ce1e80a5a40e1160f1782bfcb24b8cc5369ea9
Aki Kanellis <dkkanellis@gmail.com>
2015-12-06 16:04:31 +0000

    Add aliases

---
+++ performance/src/test/java/com/github/dkanellis/skyspark/performance/parsing/SkylineAlgorithmConverterTest.java
@@ -0,0 +1,11 @@
+package com.github.dkanellis.skyspark.performance.parsing;
+
+import org.junit.Test;
+
+public class SkylineAlgorithmConverterTest {
+
+    @Test
+    public void testConvert() throws Exception {
+
+    }
+}
