569c474ec7915ab6d4f85a31e6768b78de7eeda3
Dimitris Kanellis <dimitris@supenta.com>
2015-12-10 22:32:04 +0000

    Add bitmap basics

--- skyspark/src/main/java/com/github/dkanellis/skyspark/api/algorithms/bitmap/Bitmap.java
+++ skyspark/src/main/java/com/github/dkanellis/skyspark/api/algorithms/bitmap/Bitmap.java
@@ -1,19 +1,47 @@
 package com.github.dkanellis.skyspark.api.algorithms.bitmap;
 
 import com.github.dkanellis.skyspark.api.algorithms.SkylineAlgorithm;
-import com.github.dkanellis.skyspark.api.algorithms.bnl.FlagPointPairProducer;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
+import scala.Tuple2;
 
 import java.awt.geom.Point2D;
+import java.io.Serializable;
+import java.util.BitSet;
 import java.util.List;
 
-public class Bitmap implements SkylineAlgorithm {
-
-    private FlagPointPairProducer flagPointPairProducer;
+public class Bitmap implements SkylineAlgorithm, Serializable {
 
     @Override
     public List<Point2D> getSkylinePoints(JavaRDD<Point2D> points) {
+        JavaRDD<Double> distinctPointsOfXDimension = points
+                .map(Point2D::getX)
+                .distinct()
+                .sortBy(Double::doubleValue, false, 1);
+
+        JavaPairRDD<Double, Long> distinctPointsOfYDimension = points
+                .map(Point2D::getY)
+                .distinct()
+                .sortBy(Double::doubleValue, false, 1)
+                .zipWithIndex();
+
+
+        JavaRDD<BitSet> bitsetX = points.map(p -> getBitVectorRepresentation(p.getX(), distinctPointsOfYDimension));
+
+        List<Double> x = distinctPointsOfXDimension.collect();
+        List<Tuple2<Double, Long>> y = distinctPointsOfYDimension.collect();
+
         throw new UnsupportedOperationException("Bitmap is not supported yet.");
+    }
+
+    private BitSet getBitVectorRepresentation(final double p, JavaPairRDD<Double, Long> distinctPoints) {
+        BitSet bitSet = new BitSet();
+        bitSet.set(0, (int) getPosition(p, distinctPoints));
+        return bitSet;
+    }
+
+    private long getPosition(final double pY, JavaPairRDD<Double, Long> distinctPointsOfXDimension) {
+        return distinctPointsOfXDimension.count() - distinctPointsOfXDimension.filter(p -> p._1() >= pY).count();
     }
 
     @Override
