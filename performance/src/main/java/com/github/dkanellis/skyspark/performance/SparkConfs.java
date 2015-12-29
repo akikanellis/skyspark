d58c8b3c65c68eb1790bf2a3fb55457c19e0cbaa
Aki Kanellis <gitkanellis@gmail.com>
2015-12-29 19:55:54 +0000

    Add rounding

--- performance/src/main/java/com/github/dkanellis/skyspark/performance/SparkConfs.java
+++ performance/src/main/java/com/github/dkanellis/skyspark/performance/SparkConfs.java
@@ -3,6 +3,8 @@ package com.github.dkanellis.skyspark.performance;
 import org.apache.spark.SparkConf;
 
 import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import static com.github.dkanellis.skyspark.api.utils.Preconditions.checkNotEmpty;
 
@@ -28,12 +30,20 @@ public final class SparkConfs {
         String amountString = memory.substring(0, memory.length() - 1);
         double amount = Double.parseDouble(amountString);
 
-        if (memory.endsWith("g") || memory.endsWith("gb")) {
-            return amount;
-        } else if (memory.endsWith("m") || memory.endsWith("mb")) {
-            return amount / 1024.0;
+        if (memory.endsWith("g")) {
+            return round(amount, 2);
+        } else if (memory.endsWith("m")) {
+            return round(amount / 1024.0, 2);
         } else {
             return 0;
         }
+    }
+
+    private static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
     }
 }
