0c5590ec280898f1589f231eb6856269b3564710
Aki Kanellis <gitkanellis@gmail.com>
2016-01-03 17:24:11 +0000

    Add bitmap javadoc

--- skyspark/src/main/java/com/github/dkanellis/skyspark/api/algorithms/SkylineAlgorithm.java
+++ skyspark/src/main/java/com/github/dkanellis/skyspark/api/algorithms/SkylineAlgorithm.java
@@ -5,6 +5,19 @@ import org.apache.spark.api.java.JavaRDD;
 import java.awt.geom.Point2D;
 import java.io.Serializable;
 
+/**
+ * A skyline algorithm is an algorithm capable of calculating the skyline set of a dataset S. Current implementations
+ * include {@link com.github.dkanellis.skyspark.api.algorithms.bitmap.Bitmap},
+ * {@link com.github.dkanellis.skyspark.api.algorithms.bnl.BlockNestedLoop}
+ * and {@link com.github.dkanellis.skyspark.api.algorithms.sfs.SortFilterSkyline}
+ */
 public interface SkylineAlgorithm extends Serializable {
+
+    /**
+     * Given an RDD of points, return the skyline set of them.
+     *
+     * @param points the points to give the skyline for.
+     * @return the skyline set.
+     */
     JavaRDD<Point2D> computeSkylinePoints(JavaRDD<Point2D> points);
 }
