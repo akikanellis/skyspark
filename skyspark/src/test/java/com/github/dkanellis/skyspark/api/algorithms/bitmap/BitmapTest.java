974b946ebf6004c2969b9f96d562bc7fd077620d
Aki Kanellis <gitkanellis@gmail.com>
2016-01-03 16:30:50 +0000

    Remove extra bitmap tests

--- skyspark/src/test/java/com/github/dkanellis/skyspark/api/algorithms/bitmap/BitmapTest.java
+++ skyspark/src/test/java/com/github/dkanellis/skyspark/api/algorithms/bitmap/BitmapTest.java
@@ -2,7 +2,6 @@ package com.github.dkanellis.skyspark.api.algorithms.bitmap;
 
 import com.github.dkanellis.skyspark.api.test_utils.base.BaseSparkTest;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -14,18 +13,6 @@ public class BitmapTest extends BaseSparkTest {
     @Before
     public void setUp() {
         bitmap = new Bitmap();
-    }
-
-
-    @Test
-    @Ignore("To be implemented")
-    public void forEachXofPoint_returnBitVector() {
-
-    }
-
-    @Test
-    @Ignore("To be implemented")
-    public void forEachYofPoint_returnBitVector() {
     }
 
     @Test
