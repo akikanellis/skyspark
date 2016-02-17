440af916c4161a15e527c0a1eaaf231e522f7679
Aki Kanellis <gitkanellis@gmail.com>
2016-02-17 14:29:28 +0000

    Add equals and hashcode for Point

--- skyspark-scala/src/main/scala/com/github/dkanellis/skyspark/scala/api/algorithms/Point.scala
+++ skyspark-scala/src/main/scala/com/github/dkanellis/skyspark/scala/api/algorithms/Point.scala
@@ -9,4 +9,16 @@ class Point(dimensionValuesC: Double*) {
     Preconditions.checkPositionIndexes(1, dimensionValues.length, dimensionValues.length + 1)
     dimensionValues(dimension - 1)
   }
+
+  override def equals(other: Any): Boolean = other match {
+    case that: Point => (that canEqual this) && dimensionValues == that.dimensionValues
+    case _ => false
+  }
+
+  def canEqual(other: Any): Boolean = other.isInstanceOf[Point]
+
+  override def hashCode(): Int = {
+    val state = Seq(dimensionValues)
+    state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
+  }
 }
