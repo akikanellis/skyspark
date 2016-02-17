3fad603d4af8278f754a289a0aa413b8d909756c
Aki Kanellis <gitkanellis@gmail.com>
2016-02-17 16:22:07 +0000

    Add FlagProducer

---
+++ skyspark-scala/src/main/scala/com/github/dkanellis/skyspark/scala/api/algorithms/bnl/FlagProducer.scala
@@ -0,0 +1,17 @@
+package com.github.dkanellis.skyspark.scala.api.algorithms.bnl
+
+import com.github.dkanellis.skyspark.scala.api.algorithms.Point
+
+class FlagProducer(medianPointC: Point) {
+
+  val medianPoint = medianPointC
+
+  def calculateFlag(point: Point) = {
+    val bits = new Array[Boolean](point.size())
+    for (i <- 0 until point.size) {
+      bits(i) = point.getValueOf(i) >= medianPoint.getValueOf(i)
+    }
+
+    new Flag(bits: _*)
+  }
+}
---
+++ skyspark-scala/src/test/scala/com/github/dkanellis/skyspark/scala/api/algorithms/bnl/FlagProducerTest.scala
@@ -0,0 +1,55 @@
+package com.github.dkanellis.skyspark.scala.api.algorithms.bnl
+
+import com.github.dkanellis.skyspark.scala.api.algorithms.Point
+import org.scalatest.{BeforeAndAfterEach, FlatSpec}
+
+class FlagProducerTest extends FlatSpec with BeforeAndAfterEach {
+
+  val median = new Point(5, 5)
+  val flagProducer = new FlagProducer(median)
+
+  "A point with x and y smaller than median" should "produce flag 00" in {
+    val point = new Point(4, 4)
+    val expectedFlag = new Flag(false, false)
+
+    val actualFlag = flagProducer.calculateFlag(point)
+
+    assertResult(expectedFlag)(actualFlag)
+  }
+
+  "A point with x and y bigger than median" should "produce flag 11" in {
+    val point = new Point(6, 6)
+    val expectedFlag = new Flag(true, true)
+
+    val actualFlag = flagProducer.calculateFlag(point)
+
+    assertResult(expectedFlag)(actualFlag)
+  }
+
+  "A point with x and y equal to median" should "produce flag 11" in {
+    val point = new Point(5, 5)
+    val expectedFlag = new Flag(true, true)
+
+    val actualFlag = flagProducer.calculateFlag(point)
+
+    assertResult(expectedFlag)(actualFlag)
+  }
+
+  "A point with x bigger and y smaller than median" should "produce flag 10" in {
+    val point = new Point(6, 4)
+    val expectedFlag = new Flag(true, false)
+
+    val actualFlag = flagProducer.calculateFlag(point)
+
+    assertResult(expectedFlag)(actualFlag)
+  }
+
+  "A point with x smaller and y bigger than median" should "produce flag 01" in {
+    val point = new Point(4, 6)
+    val expectedFlag = new Flag(false, true)
+
+    val actualFlag = flagProducer.calculateFlag(point)
+
+    assertResult(expectedFlag)(actualFlag)
+  }
+}
