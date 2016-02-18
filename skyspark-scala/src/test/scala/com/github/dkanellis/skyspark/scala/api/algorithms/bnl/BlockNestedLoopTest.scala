09d3a439e702a3d2935f506a8021070bf1c2b2b4
Aki Kanellis <gitkanellis@gmail.com>
2016-02-18 14:50:06 +0000

    Use Matchers for exceptions

--- skyspark-scala/src/test/scala/com/github/dkanellis/skyspark/scala/api/algorithms/bnl/BlockNestedLoopTest.scala
+++ skyspark-scala/src/test/scala/com/github/dkanellis/skyspark/scala/api/algorithms/bnl/BlockNestedLoopTest.scala
@@ -1,23 +1,7 @@
 package com.github.dkanellis.skyspark.scala.api.algorithms.bnl
 
-import com.github.dkanellis.skyspark.scala.api.algorithms.Point
-import org.apache.spark.rdd.RDD
-import org.apache.spark.{SparkConf, SparkContext}
-import org.scalatest.{FlatSpec, PrivateMethodTester}
+import org.scalatest.{BeforeAndAfter, FlatSpec, PrivateMethodTester}
 
-class BlockNestedLoopTest extends FlatSpec with PrivateMethodTester {
+class BlockNestedLoopTest extends FlatSpec with PrivateMethodTester with BeforeAndAfter {
 
-  val sparkContext = new SparkContext(new SparkConf().setAppName("BlockNestedLoop tests").setMaster("local[*]"))
-
-  "2D points" should "produce correct median" in {
-    val pointsArray = Array(new Point(1, 1), new Point(3, 2), new Point(4, 7), new Point(9, 5))
-    val points: RDD[Point] = sparkContext.parallelize(pointsArray)
-    val expectedMedian = new Point(4.5, 3.5)
-
-    val bnl = new BlockNestedLoop
-    val decorateGetMedian = PrivateMethod[Point]('getMedian)
-    val actualMedian = bnl invokePrivate decorateGetMedian(points)
-
-    assertResult(expectedMedian)(actualMedian)
-  }
 }
--- skyspark-scala/src/test/scala/com/github/dkanellis/skyspark/scala/api/helpers/TextFileToPointRddTest.scala
+++ skyspark-scala/src/test/scala/com/github/dkanellis/skyspark/scala/api/helpers/TextFileToPointRddTest.scala
@@ -1,28 +1,22 @@
 package com.github.dkanellis.skyspark.scala.api.helpers
 
 import com.github.dkanellis.skyspark.scala.api.algorithms.Point
-import org.scalatest.{FlatSpec, PrivateMethodTester}
+import org.scalatest.{FlatSpec, Matchers, PrivateMethodTester}
 
-class TextFileToPointRddTest extends FlatSpec with PrivateMethodTester {
+class TextFileToPointRddTest extends FlatSpec with PrivateMethodTester with Matchers {
 
   val decoratePointFromTextLine = PrivateMethod[Point]('pointFromTextLine)
 
   "An empty line" should "throw an IllegalArgumentException" in {
-    intercept[IllegalArgumentException] {
-      TextFileToPointRdd invokePrivate decoratePointFromTextLine("", " ")
-    }
+    an[IllegalArgumentException] should be thrownBy (TextFileToPointRdd invokePrivate decoratePointFromTextLine("", " "))
   }
 
   "A non-number character in the line" should "throw an IllegalArgumentException" in {
-    intercept[IllegalArgumentException] {
-      TextFileToPointRdd invokePrivate decoratePointFromTextLine("1 3 a", " ")
-    }
+    an[IllegalArgumentException] should be thrownBy (TextFileToPointRdd invokePrivate decoratePointFromTextLine("1 3 a", " "))
   }
 
   "A wrong delimiter" should "throw an IllegalArgumentException" in {
-    intercept[IllegalArgumentException] {
-      TextFileToPointRdd invokePrivate decoratePointFromTextLine("1 5", ",")
-    }
+    an[IllegalArgumentException] should be thrownBy (TextFileToPointRdd invokePrivate decoratePointFromTextLine("1 5", ","))
   }
 
   "A 1 number line" should "return a 1-dimensional point" in {
