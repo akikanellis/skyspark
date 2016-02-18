454f12fecde50fa8336db210bec9019bcb45be18
Aki Kanellis <gitkanellis@gmail.com>
2016-02-18 18:09:53 +0000

    Add BlockNestedLoop

--- skyspark-scala/src/main/scala/com/github/dkanellis/skyspark/scala/api/algorithms/bnl/BlockNestedLoop.scala
+++ skyspark-scala/src/main/scala/com/github/dkanellis/skyspark/scala/api/algorithms/bnl/BlockNestedLoop.scala
@@ -5,25 +5,9 @@ import org.apache.spark.rdd.RDD
 
 class BlockNestedLoop extends SkylineAlgorithm {
 
-  var flagProducer: FlagProducer = _
-
   override def computeSkylinePoints(points: RDD[Point]): RDD[Point] = {
-    flagProducer = new FlagProducer(getMedian(points))
+    val localSkylinesWithFlags = Divider.divide(points)
 
     throw new UnsupportedOperationException("WIP")
-  }
-
-  private def getMedian(points: RDD[Point]) = {
-    val dimensions = points.take(1)(0).size()
-
-    val medianDimensionValues = new Array[Double](dimensions)
-    for (i <- 0 until dimensions) {
-      medianDimensionValues(i) = points.max()(new Ordering[Point]() {
-        override def compare(first: Point, second: Point): Int =
-          Ordering[Double].compare(first.getValueOf(i), second.getValueOf(i))
-      }).getValueOf(i)
-    }
-
-    new Point(medianDimensionValues: _*)
   }
 }
