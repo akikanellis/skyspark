8ea9d4662a8155a8083fa0fdf95fcc2cf13e8b6d
Aki Kanellis <gitkanellis@gmail.com>
2016-02-19 13:52:02 +0000

    Add Merger

---
+++ skyspark-scala/src/main/scala/com/github/dkanellis/skyspark/scala/api/algorithms/bnl/Merger.scala
@@ -0,0 +1,15 @@
+package com.github.dkanellis.skyspark.scala.api.algorithms.bnl
+
+import com.github.dkanellis.skyspark.scala.api.algorithms.Point
+import org.apache.spark.rdd.RDD
+
+object Merger {
+
+  def merge(flagsWithPoints: RDD[(Flag, Point)]): RDD[Point] = {
+    val inSinglePartition = flagsWithPoints
+      .coalesce(1)
+      .glom()
+
+    inSinglePartition.flatMap(fp => BnlAlgorithm.computeSkylinesWithPreComparison(fp))
+  }
+}
---
+++ skyspark-scala/src/test/scala/com/github/dkanellis/skyspark/scala/api/algorithms/bnl/MergerTest.scala
@@ -0,0 +1,40 @@
+package com.github.dkanellis.skyspark.scala.api.algorithms.bnl
+
+import com.github.dkanellis.skyspark.scala.api.algorithms.Point
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
+
+class MergerTest extends FlatSpec with BeforeAndAfter with Matchers {
+
+  private var flagPoints: RDD[(Flag, Point)] = _
+  private var sc: SparkContext = _
+
+  before {
+    val flagPointsSeq = Seq(
+      (new Flag(true, true), new Point(5.9, 4.6)),
+      (new Flag(true, false), new Point(5.0, 4.1)), (new Flag(true, false), new Point(5.9, 4.0)),
+      (new Flag(true, false), new Point(6.7, 3.3)), (new Flag(true, false), new Point(6.1, 3.4)),
+      (new Flag(false, true), new Point(2.5, 7.3)))
+
+    val sparkConf = new SparkConf().setAppName("MedianFinder tests").setMaster("local[*]")
+    sc = new SparkContext(sparkConf)
+
+    flagPoints = sc.parallelize(flagPointsSeq)
+  }
+
+  after {
+    if (sc != null) {
+      sc.stop()
+    }
+  }
+  "A set of flag-points" should "keep only the skylines and be merged" in {
+    val expectedSkylines = Seq(
+      new Point(5.0, 4.1), new Point(5.9, 4.0), new Point(2.5, 7.3), new Point(6.7, 3.3),
+      new Point(6.1, 3.4))
+
+    val actualSkylines = Merger.merge(flagPoints)
+
+    actualSkylines.collect() should contain theSameElementsAs expectedSkylines
+  }
+}
