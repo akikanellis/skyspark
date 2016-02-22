0b6e199a43d6addcbf213ef6aa8712bad621fa3b
Aki Kanellis <gitkanellis@gmail.com>
2016-02-22 14:44:26 +0000

    Update tests

--- skyspark-scala/src/test/scala/com/github/dkanellis/skyspark/scala/api/algorithms/bnl/MedianFinderTest.scala
+++ skyspark-scala/src/test/scala/com/github/dkanellis/skyspark/scala/api/algorithms/bnl/MedianFinderTest.scala
@@ -2,15 +2,18 @@ package com.github.dkanellis.skyspark.scala.api.algorithms.bnl
 
 import com.github.dkanellis.skyspark.scala.api.algorithms.Point
 import org.apache.spark.{SparkConf, SparkContext}
-import org.scalatest.{BeforeAndAfter, FlatSpec, PrivateMethodTester}
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers, PrivateMethodTester}
 
-class MedianFinderTest extends FlatSpec with PrivateMethodTester with BeforeAndAfter {
+class MedianFinderTest extends FlatSpec with PrivateMethodTester with BeforeAndAfter with Matchers {
 
   private var sc: SparkContext = _
+  private var medianFinder: MedianFinder = _
 
   before {
     val sparkConf = new SparkConf().setAppName("MedianFinder tests").setMaster("local[*]")
     sc = new SparkContext(sparkConf)
+
+    medianFinder = new MedianFinder
   }
 
   after {
@@ -19,12 +22,25 @@ class MedianFinderTest extends FlatSpec with PrivateMethodTester with BeforeAndA
     }
   }
 
+  "A zero dimension size" should "throw an IllegalStateException" in {
+    medianFinder.numberOfDimensions = 0
+
+    an[IllegalStateException] should be thrownBy medianFinder.getMedian(null)
+  }
+
+  "A negative dimension size" should "throw an IllegalStateException" in {
+    medianFinder.numberOfDimensions = -1
+
+    an[IllegalStateException] should be thrownBy medianFinder.getMedian(null)
+  }
+
   "2D points" should "produce correct median" in {
     val pointsArray = Seq[Point](new Point(1, 1), new Point(3, 2), new Point(4, 7), new Point(9, 5))
     val points = sc.parallelize(pointsArray)
+    medianFinder.numberOfDimensions = pointsArray.head.size()
     val expectedMedian = new Point(4.5, 3.5)
 
-    val actualMedian = MedianFinder.getMedian(points, 2)
+    val actualMedian = medianFinder.getMedian(points)
 
     assertResult(expectedMedian)(actualMedian)
   }
@@ -32,9 +48,10 @@ class MedianFinderTest extends FlatSpec with PrivateMethodTester with BeforeAndA
   "3D points" should "produce correct median" in {
     val pointsArray = Seq[Point](new Point(1, 1, 5), new Point(3, 2, 3), new Point(4, 7, 1), new Point(9, 5, 8))
     val points = sc.parallelize(pointsArray)
+    medianFinder.numberOfDimensions = pointsArray.head.size()
     val expectedMedian = new Point(4.5, 3.5, 4)
 
-    val actualMedian = MedianFinder.getMedian(points, 3)
+    val actualMedian = medianFinder.getMedian(points)
 
     assertResult(expectedMedian)(actualMedian)
   }
