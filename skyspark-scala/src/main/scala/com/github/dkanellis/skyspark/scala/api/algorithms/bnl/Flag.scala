cc6a11fbd2bed59f0b4821ec4fac2d73b6c0e262
Aki Kanellis <gitkanellis@gmail.com>
2016-02-22 13:42:57 +0000

    Change Flag to case class

--- skyspark-scala/src/main/scala/com/github/dkanellis/skyspark/scala/api/algorithms/bnl/Flag.scala
+++ skyspark-scala/src/main/scala/com/github/dkanellis/skyspark/scala/api/algorithms/bnl/Flag.scala
@@ -1,31 +1,8 @@
 package com.github.dkanellis.skyspark.scala.api.algorithms.bnl
 
-import com.google.common.base.Preconditions
-
-class Flag(bitsC: Boolean*) extends Serializable {
-
-  val bits = bitsC
+case class Flag(bits: Boolean*) {
 
   def size = bits.length
 
-  def getValueOf(dimension: Int) = {
-    Preconditions.checkElementIndex(dimension, bits.length)
-    bits(dimension)
-  }
-
-  override def equals(other: Any): Boolean = other match {
-    case that: Flag =>
-      (that canEqual this) &&
-        bits == that.bits
-    case _ => false
-  }
-
-  def canEqual(other: Any): Boolean = other.isInstanceOf[Flag]
-
-  override def hashCode(): Int = {
-    val state = Seq(bits)
-    state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
-  }
-
-  override def toString = s"Flag($bits)"
+  def bit(i: Int) = bits(i)
 }
