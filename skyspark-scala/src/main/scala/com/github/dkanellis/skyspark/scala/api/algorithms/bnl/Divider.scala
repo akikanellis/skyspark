2c7e4d356696828c68b588660b4a47a91339d3a1
Aki Kanellis <gitkanellis@gmail.com>
2016-02-22 14:32:03 +0000

    Change Divider from object to class

--- skyspark-scala/src/main/scala/com/github/dkanellis/skyspark/scala/api/algorithms/bnl/Divider.scala
+++ skyspark-scala/src/main/scala/com/github/dkanellis/skyspark/scala/api/algorithms/bnl/Divider.scala
@@ -1,13 +1,17 @@
 package com.github.dkanellis.skyspark.scala.api.algorithms.bnl
 
 import com.github.dkanellis.skyspark.scala.api.algorithms.Point
+import com.google.common.base.Preconditions
 import org.apache.spark.rdd.RDD
 
-private[bnl] object Divider {
+private[bnl] class Divider {
+
+  private[bnl] var numberOfDimensions = 0
 
   private[bnl] def divide(points: RDD[Point]): RDD[(Flag, Point)] = {
-    val numOfDimensions = points.first().size()
-    val median = MedianFinder.getMedian(points, numOfDimensions)
+    Preconditions.checkState(numberOfDimensions > 0, "Dimensionality can't be less than 1.", null)
+
+    val median = MedianFinder.getMedian(points, numberOfDimensions)
     val flagProducer = new FlagProducer(median)
 
     val flagPoints = points.map(p => (flagProducer.calculateFlag(p), p))
