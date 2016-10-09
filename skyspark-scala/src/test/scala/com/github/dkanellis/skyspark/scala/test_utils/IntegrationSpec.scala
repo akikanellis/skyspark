3f922f4b3d7b9b4a600d65faa1f1d5cd0cfce869
Aki Kanellis <me@akikanellis.com>
2016-10-09 23:50:24 +0100

    Add Unit and Integration specs

---
+++ skyspark-scala/src/test/scala/com/github/dkanellis/skyspark/scala/test_utils/IntegrationSpec.scala
@@ -0,0 +1,6 @@
+package com.github.dkanellis.skyspark.scala.test_utils
+
+import com.github.dkanellis.skyspark.scala.api.SparkAddOn
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
+
+class IntegrationSpec extends FlatSpec with BeforeAndAfter with Matchers with SparkAddOn
