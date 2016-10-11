bdb99093c6d4d37acb912e4f219a192799e931fe
Aki Kanellis <me@akikanellis.com>
2016-10-11 21:31:25 +0100

    Fix Spark logs

--- skyspark-scala/src/test/scala/com/github/dkanellis/skyspark/scala/api/SparkAddOn.scala
+++ skyspark-scala/src/test/scala/com/github/dkanellis/skyspark/scala/api/SparkAddOn.scala
@@ -1,19 +1,27 @@
 package com.github.dkanellis.skyspark.scala.api
 
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.{SparkConf, SparkContext}
 
 trait SparkAddOn {
-
-  val conf = new SparkConf()
+  private val conf = new SparkConf()
     .setMaster("local[*]")
     .setAppName("spark-tests")
 
   def withSpark(f: SparkContext => Unit) = {
+    setLogLevels(Level.WARN)
     val sc: SparkContext = new SparkContext(conf)
+
     try {
       f(sc)
     } finally {
       sc.stop()
     }
+  }
+
+  private def setLogLevels(level: Level) {
+    Logger.getRootLogger.setLevel(level)
+    Logger.getLogger("org").setLevel(level)
+    Logger.getLogger("akka").setLevel(level)
   }
 }
