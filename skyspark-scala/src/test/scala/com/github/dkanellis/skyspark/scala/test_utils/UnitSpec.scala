6f50629d96d4413b8ffc3dd76a0e7bd6c2b80e7d
Aki Kanellis <me@akikanellis.com>
2016-10-12 22:44:33 +0100

    Add UnitSpec

---
+++ skyspark-scala/src/test/scala/com/github/dkanellis/skyspark/scala/test_utils/UnitSpec.scala
@@ -0,0 +1,6 @@
+package com.github.dkanellis.skyspark.scala.test_utils
+
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
+
+class UnitSpec extends FlatSpec with BeforeAndAfter with Matchers with MockitoSugar
