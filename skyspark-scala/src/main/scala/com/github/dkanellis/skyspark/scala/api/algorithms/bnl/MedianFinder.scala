79168c492cfc9a94e7c00fcc3d3459d2d6b37660
Aki Kanellis <me@akikanellis.com>
2016-10-12 15:00:15 +0100

    Add local computation of median

--- skyspark-scala/src/main/scala/com/github/dkanellis/skyspark/scala/api/algorithms/bnl/MedianFinder.scala
+++ skyspark-scala/src/main/scala/com/github/dkanellis/skyspark/scala/api/algorithms/bnl/MedianFinder.scala
@@ -1,7 +1,6 @@
 package com.github.dkanellis.skyspark.scala.api.algorithms.bnl
 
 import com.github.dkanellis.skyspark.scala.api.algorithms.Point
-import com.google.common.base.Preconditions
 import org.apache.spark.rdd.RDD
 
 private[bnl] class MedianFinder extends Serializable {
@@ -9,7 +8,7 @@ private[bnl] class MedianFinder extends Serializable {
   private[bnl] var numberOfDimensions = 0
 
   private[bnl] def getMedian(points: RDD[Point]): Point = {
-    Preconditions.checkState(numberOfDimensions > 0, "Dimensionality can't be less than 1.", null)
+    val numberOfDimensions = points.first().size()
 
     val medianDimensionValues = new Array[Double](numberOfDimensions)
     for (i <- 0 until numberOfDimensions) {
