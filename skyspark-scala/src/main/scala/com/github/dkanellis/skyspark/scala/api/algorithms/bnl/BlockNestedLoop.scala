9a0ff2184934ea9959ea955874c22f84e1261ff5
Aki Kanellis <me@akikanellis.com>
2016-10-12 14:55:47 +0100

    Add constructor injection to BNL

--- skyspark-scala/src/main/scala/com/github/dkanellis/skyspark/scala/api/algorithms/bnl/BlockNestedLoop.scala
+++ skyspark-scala/src/main/scala/com/github/dkanellis/skyspark/scala/api/algorithms/bnl/BlockNestedLoop.scala
@@ -3,10 +3,11 @@ package com.github.dkanellis.skyspark.scala.api.algorithms.bnl
 import com.github.dkanellis.skyspark.scala.api.algorithms.{Point, SkylineAlgorithm}
 import org.apache.spark.rdd.RDD
 
-class BlockNestedLoop extends SkylineAlgorithm {
+class BlockNestedLoop(divider: Divider, merger: Merger) extends SkylineAlgorithm {
 
-  val divider = new Divider
-  val merger = new Merger
+  def this() = {
+    this(new Divider, new Merger)
+  }
 
   override def computeSkylinePoints(points: RDD[Point]): RDD[Point] = {
     divider.numberOfDimensions = points.first().size()
