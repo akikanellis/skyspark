cb0960728dd5f7cdc70c567daa8d1b66610b1f49
Aki Kanellis <me@akikanellis.com>
2016-10-14 20:58:52 +0100

    Refactor BnlAlgorithm

--- skyspark-scala/src/test/scala/com/github/dkanellis/skyspark/scala/api/algorithms/bnl/BnlAlgorithmTest.scala
+++ skyspark-scala/src/test/scala/com/github/dkanellis/skyspark/scala/api/algorithms/bnl/BnlAlgorithmTest.scala
@@ -4,8 +4,7 @@ import com.github.dkanellis.skyspark.scala.api.algorithms.Point
 import com.github.dkanellis.skyspark.scala.test_utils.UnitSpec
 
 class BnlAlgorithmTest extends UnitSpec {
-
-  private var bnlAlgorithm: BnlAlgorithm = _
+  var bnlAlgorithm: BnlAlgorithm = _
 
   before {
     bnlAlgorithm = new BnlAlgorithm
@@ -13,12 +12,9 @@ class BnlAlgorithmTest extends UnitSpec {
 
   "Without pre-comparisson a set of points" should "compute the skylines" in {
     val points = Seq(
-      Point(5.4, 4.4), Point(5.0, 4.1), Point(3.6, 9.0), Point(5.9, 4.0),
-      Point(5.9, 4.6), Point(2.5, 7.3), Point(6.3, 3.5), Point(9.9, 4.1),
-      Point(6.7, 3.3), Point(6.1, 3.4))
-    val expectedSkylines = Seq(
-      Point(5.0, 4.1), Point(5.9, 4.0), Point(2.5, 7.3), Point(6.7, 3.3),
-      Point(6.1, 3.4))
+      Point(5.4, 4.4), Point(5.0, 4.1), Point(3.6, 9.0), Point(5.9, 4.0), Point(5.9, 4.6), Point(2.5, 7.3),
+      Point(6.3, 3.5), Point(9.9, 4.1), Point(6.7, 3.3), Point(6.1, 3.4))
+    val expectedSkylines = Seq(Point(5.0, 4.1), Point(5.9, 4.0), Point(2.5, 7.3), Point(6.7, 3.3), Point(6.1, 3.4))
 
     val actualSkylines = bnlAlgorithm.computeSkylinesWithoutPreComparison(points)
 
@@ -26,9 +22,7 @@ class BnlAlgorithmTest extends UnitSpec {
   }
 
   "Without pre-comparisson a set of only skylines" should "have nothing changed" in {
-    val points = Seq(
-      Point(5.0, 4.1), Point(5.9, 4.0), Point(2.5, 7.3), Point(6.7, 3.3),
-      Point(6.1, 3.4))
+    val points = Seq(Point(5.0, 4.1), Point(5.9, 4.0), Point(2.5, 7.3), Point(6.7, 3.3), Point(6.1, 3.4))
 
     val actualSkylines = bnlAlgorithm.computeSkylinesWithoutPreComparison(points)
 
@@ -37,13 +31,10 @@ class BnlAlgorithmTest extends UnitSpec {
 
   "With pre-comparisson a set of points with their flags" should "compute the skylines" in {
     val flagsPoints = Seq(
-      (Flag(true, true), Point(5.9, 4.6)),
-      (Flag(true, false), Point(5.0, 4.1)), (Flag(true, false), Point(5.9, 4.0)),
-      (Flag(true, false), Point(6.7, 3.3)), (Flag(true, false), Point(6.1, 3.4)),
-      (Flag(false, true), Point(2.5, 7.3)))
-    val expectedSkylines = Seq(
-      Point(5.0, 4.1), Point(5.9, 4.0), Point(2.5, 7.3), Point(6.7, 3.3),
-      Point(6.1, 3.4))
+      (Flag(true, true), Point(5.9, 4.6)), (Flag(true, false), Point(5.0, 4.1)),
+      (Flag(true, false), Point(5.9, 4.0)), (Flag(true, false), Point(6.7, 3.3)),
+      (Flag(true, false), Point(6.1, 3.4)), (Flag(false, true), Point(2.5, 7.3)))
+    val expectedSkylines = Seq(Point(5.0, 4.1), Point(5.9, 4.0), Point(2.5, 7.3), Point(6.7, 3.3), Point(6.1, 3.4))
 
     val actualSkylines = bnlAlgorithm.computeSkylinesWithPreComparison(flagsPoints)
 
@@ -52,13 +43,9 @@ class BnlAlgorithmTest extends UnitSpec {
 
   "With pre-comparisson a set of only skylines with their flags" should "have nothing changed" in {
     val flagsPoints = Seq(
-      (Flag(true, false), Point(5.0, 4.1)), (Flag(true, false), Point(5.9, 4.0)),
-      (Flag(true, false), Point(6.7, 3.3)), (Flag(true, false), Point(6.1, 3.4)),
-      (Flag(false, true), Point(2.5, 7.3)))
-    val expectedSkylines = Seq(
-      Point(5.0, 4.1), Point(5.9, 4.0),
-      Point(6.7, 3.3), Point(6.1, 3.4),
-      Point(2.5, 7.3))
+      (Flag(true, false), Point(5.0, 4.1)), (Flag(true, false), Point(5.9, 4.0)), (Flag(true, false), Point(6.7, 3.3)),
+      (Flag(true, false), Point(6.1, 3.4)), (Flag(false, true), Point(2.5, 7.3)))
+    val expectedSkylines = Seq(Point(5.0, 4.1), Point(5.9, 4.0), Point(6.7, 3.3), Point(6.1, 3.4), Point(2.5, 7.3))
 
     val actualSkylines = bnlAlgorithm.computeSkylinesWithPreComparison(flagsPoints)
 
