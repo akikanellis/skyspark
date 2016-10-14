db65f8656fdc056ffe7a7e91cb2399554264b25a
Aki Kanellis <me@akikanellis.com>
2016-10-14 18:57:23 +0100

    Refactor dominating algorithm

--- skyspark-scala/src/main/scala/com/github/dkanellis/skyspark/scala/api/algorithms/DominatingAlgorithm.scala
+++ skyspark-scala/src/main/scala/com/github/dkanellis/skyspark/scala/api/algorithms/DominatingAlgorithm.scala
@@ -2,18 +2,24 @@ package com.github.dkanellis.skyspark.scala.api.algorithms
 
 private[algorithms] object DominatingAlgorithm {
 
-  private[algorithms] def dominates(first: Point, second: Point): Boolean = {
+  private[algorithms] def dominates(candidate: Point, other: Point): Boolean = {
     var atLeastOneSmaller = false
-    for (i <- 0 until first.size()) {
-      if (first.dimension(i) > second.dimension(i)) {
-        return false
-      }
 
-      if (first.dimension(i) < second.dimension(i)) {
-        atLeastOneSmaller = true
-      }
+    for (i <- 0 until candidate.size) {
+      val candidateDimension = candidate.dimension(i)
+      val otherDimension = other.dimension(i)
+
+      if (candidateDimensionIsWorse(candidateDimension, otherDimension)) return false
+
+      if (candidateDimensionIsBetter(candidateDimension, otherDimension)) atLeastOneSmaller = true
     }
 
     atLeastOneSmaller
   }
+
+  private def candidateDimensionIsWorse(candidateDimension: Double, otherDimension: Double): Boolean =
+    candidateDimension > otherDimension
+
+  private def candidateDimensionIsBetter(candidateDimension: Double, otherDimension: Double): Boolean =
+    candidateDimension < otherDimension
 }
