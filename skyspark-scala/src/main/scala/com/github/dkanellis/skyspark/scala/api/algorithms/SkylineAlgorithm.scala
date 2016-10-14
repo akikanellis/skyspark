afa52a0f3c39f3ebf0198c7a21c094619cff727a
Aki Kanellis <me@akikanellis.com>
2016-10-14 19:26:47 +0100

    Add Skyline trait docs

--- skyspark-scala/src/main/scala/com/github/dkanellis/skyspark/scala/api/algorithms/SkylineAlgorithm.scala
+++ skyspark-scala/src/main/scala/com/github/dkanellis/skyspark/scala/api/algorithms/SkylineAlgorithm.scala
@@ -2,7 +2,17 @@ package com.github.dkanellis.skyspark.scala.api.algorithms
 
 import org.apache.spark.rdd.RDD
 
+/**
+  * A skyline algorithm is an algorithm capable of calculating the skyline set of a dataset S. Current implementations
+  * include [[com.github.dkanellis.skyspark.scala.api.algorithms.bnl.BlockNestedLoop]].
+  */
 trait SkylineAlgorithm extends Serializable {
 
+  /**
+    * Given an RDD of points, return their skyline set.
+    *
+    * @param points the points to compute the skyline for.
+    * @return the skyline set.
+    */
   def computeSkylinePoints(points: RDD[Point]): RDD[Point]
 }
