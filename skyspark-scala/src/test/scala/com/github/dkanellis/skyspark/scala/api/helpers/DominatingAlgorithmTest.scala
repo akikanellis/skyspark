458e4aaef05de50a7f16bafb569133b3f4083d49
Aki Kanellis <me@akikanellis.com>
2016-10-14 18:42:09 +0100

    Add dominating algorithm

--- skyspark-scala/src/test/scala/com/github/dkanellis/skyspark/scala/api/helpers/DominatingAlgorithmTest.scala
+++ skyspark-scala/src/test/scala/com/github/dkanellis/skyspark/scala/api/helpers/DominatingAlgorithmTest.scala
@@ -1,42 +1,42 @@
 package com.github.dkanellis.skyspark.scala.api.helpers
 
-import com.github.dkanellis.skyspark.scala.api.algorithms.Point
+import com.github.dkanellis.skyspark.scala.api.algorithms.{DominatingAlgorithm, Point}
 import com.github.dkanellis.skyspark.scala.test_utils.UnitSpec
 
-class PointsTest extends UnitSpec {
+class DominatingAlgorithmTest extends UnitSpec {
 
   "A point with one dimension smaller" should "dominate when the rest dimensions are smaller" in {
     val dominator = Point(2, 4, 5)
     val dominatable = Point(3, 5, 6)
 
-    Points.dominates(dominator, dominatable) shouldBe true
+    DominatingAlgorithm.dominates(dominator, dominatable) shouldBe true
   }
 
   it should "dominate when the rest dimensions are equal" in {
     val first = Point(2, 4, 5)
     val second = Point(3, 4, 5)
 
-    Points.dominates(first, second) shouldBe true
+    DominatingAlgorithm.dominates(first, second) shouldBe true
   }
 
   it should "not dominate when at least one dimension is bigger" in {
     val first = Point(2, 4, 7)
     val second = Point(3, 5, 6)
 
-    Points.dominates(first, second) shouldBe false
+    DominatingAlgorithm.dominates(first, second) shouldBe false
   }
 
   "A point with all dimensions bigger" should "not dominate" in {
     val first = Point(4, 6, 7)
     val second = Point(3, 5, 6)
 
-    Points.dominates(first, second) shouldBe false
+    DominatingAlgorithm.dominates(first, second) shouldBe false
   }
 
   "A point with all dimensions equal" should "not dominate" in {
     val first = Point(2, 4, 5)
     val second = Point(2, 4, 5)
 
-    Points.dominates(first, second) shouldBe false
+    DominatingAlgorithm.dominates(first, second) shouldBe false
   }
 }
