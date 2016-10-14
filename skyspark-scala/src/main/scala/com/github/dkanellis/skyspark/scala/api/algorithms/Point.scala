4c8f9af347f7651bf01d982c91e155f5bc34879e
Aki Kanellis <me@akikanellis.com>
2016-10-14 19:30:39 +0100

    Add Point doc

--- skyspark-scala/src/main/scala/com/github/dkanellis/skyspark/scala/api/algorithms/Point.scala
+++ skyspark-scala/src/main/scala/com/github/dkanellis/skyspark/scala/api/algorithms/Point.scala
@@ -1,10 +1,20 @@
 package com.github.dkanellis.skyspark.scala.api.algorithms
 
+/**
+  * Defines a point representing a location in a coordinate space whose size is defined by the number of dimensions
+  * provided.
+  */
 case class Point(private val dimensions: Double*) extends Serializable {
 
   def size() = dimensions.length
 
   def dimension(i: Int) = dimensions(i)
 
+  /**
+    * Checks if this point dominates another one by using the specified dominating algorithm.
+    *
+    * @param other The point to check against
+    * @return True if this point dominates the other, false if not
+    */
   def dominates(other: Point): Boolean = DominatingAlgorithm.dominates(this, other)
 }
