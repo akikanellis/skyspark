cc1eb06722a4656a0778e340c103e14914269f2c
Aki Kanellis <me@akikanellis.com>
2016-10-14 19:20:49 +0100

    Add docs to text file converter

--- skyspark-scala/src/main/scala/com/github/dkanellis/skyspark/scala/api/helpers/TextFileToPointRdd.scala
+++ skyspark-scala/src/main/scala/com/github/dkanellis/skyspark/scala/api/helpers/TextFileToPointRdd.scala
@@ -4,8 +4,19 @@ import com.github.dkanellis.skyspark.scala.api.algorithms.Point
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 
+/**
+  * Helper for converting a text file to a point RDD. The file should have one point per line.
+  */
 object TextFileToPointRdd {
 
+  /**
+    * Converts the text file to a point RDD.
+    *
+    * @param sparkContext An active Spark context
+    * @param filePath     The full path to the file
+    * @param delimiter    Any single character except a period
+    * @return The constructed point RDD
+    */
   def convert(sparkContext: SparkContext, filePath: String, delimiter: String): RDD[Point] = {
     sparkContext.textFile(filePath)
       .map(pointFromTextLine(_, delimiter))
