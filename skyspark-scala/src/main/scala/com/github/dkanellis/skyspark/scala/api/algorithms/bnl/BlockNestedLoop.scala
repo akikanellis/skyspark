7a1909a971acebf7ed412d4eebe3131fb6f627e7
Aki Kanellis <me@akikanellis.com>
2016-10-14 20:09:34 +0100

    Refactor BlockNestedLoop

--- skyspark-scala/src/main/scala/com/github/dkanellis/skyspark/scala/api/algorithms/bnl/BlockNestedLoop.scala
+++ skyspark-scala/src/main/scala/com/github/dkanellis/skyspark/scala/api/algorithms/bnl/BlockNestedLoop.scala
@@ -3,11 +3,18 @@ package com.github.dkanellis.skyspark.scala.api.algorithms.bnl
 import com.github.dkanellis.skyspark.scala.api.algorithms.{Point, SkylineAlgorithm}
 import org.apache.spark.rdd.RDD
 
-class BlockNestedLoop(divider: Divider, merger: Merger) extends SkylineAlgorithm {
+/**
+  * The block-nested loop (BNL) algorithm.
+  * <p>
+  * First we do the division job of the points to <Flag, Point> and then we merge the local skylines together and
+  * calculate the total skyline set.
+  * <p>
+  * For more information read <i>Adapting Skyline Computation to the MapReduce Framework: Algorithms and Experiments</i>
+  * by <i>Boliang Zhang, Shuigeng Zhou, Jihong Guan</i>
+  */
+class BlockNestedLoop(private val divider: Divider, private val merger: Merger) extends SkylineAlgorithm {
 
-  def this() = {
-    this(new Divider(new BnlAlgorithm), new Merger)
-  }
+  def this() = this(new Divider(new BnlAlgorithm), new Merger)
 
   override def computeSkylinePoints(points: RDD[Point]): RDD[Point] = {
     require(!points.isEmpty)
